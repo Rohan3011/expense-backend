36cbcd7e587ee7d591b0d091d4b631a63e0dad5a
rohan3011 <rohankamble3011@gmail.com>
2023-07-05 17:35:02 +0530

    try again 🥲

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -5,6 +5,18 @@ async function requireUser(req: Request, res: Response, next: NextFunction) {
   const user = res.locals.user;
 
   if (!user) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL!);
+
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+
     return res
       .status(HttpStatusCode.FORBIDDEN)
       .send({ error: "User doesn't exits, please login!" });
